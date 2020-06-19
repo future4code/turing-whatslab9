ec4c838bbc860288ec8bda701bc6c3519e35fc31
Diego Miyabara <diego.miyabara@hotmail.com>
2020-06-19 14:12:02 -0300

    Implementação de funcionalidades whatslab

--- whatslab9/src/App.js
+++ whatslab9/src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CampoMensagem = styled.div`
+height: 97%;
+display: flex;
+flex-direction: column-reverse;
+padding-left: 10px;
+`
+
+const CampoFuncionalidades = styled.div`
+width: 100%;
+`
+const CampoInput = styled.input`
+margin:0;
+padding: 0;
+
+`
+const CampoBotao = styled.button`
+margin:0;
+padding: 0;
+
+`
+class App extends React.Component {
+  state = {
+    mensagens: [
+     
+    ],
+    valorInputUsuario: "",
+    valorInputTexto: ""
+  };
+
+  
+
+  adicionaMensagem = () => {
+    const novaMensagem = {
+      usuario: this.state.valorInputUsuario,
+      texto: this.state.valorInputTexto
+    }
+    const novasMensagens = [...this.state.mensagens, novaMensagem]
+    this.setState({mensagens: novasMensagens});
+    this.setState({valorInputTexto: ""})
+  }
+
+  onChangeInputUsuario = event => {
+    this.setState({ valorInputUsuario: event.target.value});
+  }
+  onChangeInputTexto = event => {
+    this.setState({ valorInputTexto: event.target.value });
+  }
+
+  render(){
+    const listaDeMensagens = this.state.mensagens.map(mensagem => {
+      return (
+      <div>
+        {mensagem.usuario}: {mensagem.texto}
+      </div>
+      )
+    })
+
+    return (
+      <div className="AppTotal">
+        <div className="App">
+        <CampoMensagem>
+          {listaDeMensagens}
+        </CampoMensagem>
+          <CampoFuncionalidades>
+            <CampoInput 
+            placeholder="Usuário" 
+            value={this.state.valorInputUsuario} 
+            onChange={this.onChangeInputUsuario}
+            />
+            <CampoInput 
+            placeholder="Mensagem"
+            value= {this.state.valorInputTexto} 
+            onChange= {this.onChangeInputTexto}
+            />
+            <button onClick={this.adicionaMensagem}>Enviar</button>
+          </CampoFuncionalidades>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
